187cd39aeb46d02b71b8eda07f8c6449b25c7d3c
Rudy <lay971@hotmail.fr>
2021-02-06 16:48:04 +0100

    Modification graphique et stu diverses
    + navbar
    + page À propos
    + page des playlists : fond gris sous la liste des chartes

--- src/Form/ChartFormType.php
+++ src/Form/ChartFormType.php
@@ -17,12 +17,12 @@ class ChartFormType extends AbstractType
         $builder
             ->add('url', UrlType::class, [
                 'attr' => [
-                    'placeholder'   => 'Ex: https://www.billboard.com/charts/hot-100',
+                    'placeholder'   => 'Ex: www.billboard.com/charts/hot-100',
                     'class'         => 'home_input',
                 ],
             ])
             ->add('save', SubmitType::class, [
-                'label' => 'Playlister',
+                'label' => 'Scanner',
                 'attr'  => [
                     'class' => 'home_form_button button_fill'
                 ],
